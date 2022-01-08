de596a9edfde5de6d8a541d4c8b922b76600da63
volskyi.dmytro <volskyi.dmytro@gmail.com>
2022-01-08 22:25:09 +0200

    Initial commit

---
+++ src/main/java/com/example/kubernetescats/KubernetesCatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kubernetescats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesCatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesCatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kubernetescats/KubernetesCatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kubernetescats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesCatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
